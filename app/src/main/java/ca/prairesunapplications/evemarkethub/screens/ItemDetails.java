f646833ac8a313199982292bb2edd6b5f0769726
JediMasterFluff <fluffyman35@gmail.com>
2017-11-13 10:39:08 -0800

    Added ItemList and ItemDetail screens

---
+++ app/src/main/java/ca/prairesunapplications/evemarkethub/screens/ItemDetails.java
@@ -0,0 +1,15 @@
+package ca.prairesunapplications.evemarkethub.screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.prairesunapplications.evemarkethub.R;
+
+public class ItemDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_details);
+    }
+}
--- app/src/main/java/ca/prairesunapplications/evemarkethub/screens/MainActivity.java
+++ app/src/main/java/ca/prairesunapplications/evemarkethub/screens/MainActivity.java
@@ -1,7 +1,9 @@
-package ca.prairesunapplications.evemarkethub;
+package ca.prairesunapplications.evemarkethub.screens;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import ca.prairesunapplications.evemarkethub.R;
 
 public class MainActivity extends AppCompatActivity {
 
