a2a7083d2e1b9ca5bc94c113c76c0b2f9ea79d68
JediMasterFluff <fluffyman35@gmail.com>
2018-11-18 23:02:55 -0800

    New Launcher icon and updated rest api calls

--- app/src/main/java/ca/prairesunapplications/evemarkethub/utils/AppController.java
+++ app/src/main/java/ca/prairesunapplications/evemarkethub/utils/AppController.java
@@ -10,50 +10,55 @@ import com.android.volley.toolbox.Volley;
 
 public class AppController extends Application {
 
-    public static final String TAG = AppController.class.getSimpleName();
+    public static final String TAG = AppController.class
+            .getSimpleName();
 
     private RequestQueue mRequestQueue;
     private ImageLoader mImageLoader;
 
-    private static AppController instance;
+    private static AppController mInstance;
 
     @Override
-    public void onCreate(){
+    public void onCreate() {
         super.onCreate();
-        instance = this;
+        mInstance = this;
     }
 
-    public static  synchronized AppController getInstance(){
-        return instance;
+    public static synchronized AppController getInstance() {
+        return mInstance;
     }
 
-    public RequestQueue getRequestQueue(){
-        if(mRequestQueue == null)
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
             mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+        }
 
         return mRequestQueue;
     }
 
-    public ImageLoader getmImageLoader(){
+    public ImageLoader getImageLoader() {
         getRequestQueue();
-        if(mImageLoader == null)
-            mImageLoader = new ImageLoader(this.mRequestQueue, new LruBitmapCache());
-
+        if (mImageLoader == null) {
+            mImageLoader = new ImageLoader(this.mRequestQueue,
+                    new LruBitmapCache());
+        }
         return this.mImageLoader;
     }
 
-    public <T> void addToRequestQueue(Request<T> req, String tag){
-        req.setTag(TextUtils.isEmpty(tag)? TAG : tag);
+    public <T> void addToRequestQueue(Request<T> req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
         getRequestQueue().add(req);
     }
 
-    public <T> void addToRequestQueue(Request <T> req){
+    public <T> void addToRequestQueue(Request<T> req) {
         req.setTag(TAG);
         getRequestQueue().add(req);
     }
 
-    public void cancelPendingRequests(Object tag){
-        if(mRequestQueue != null)
-            mRequestQueue.cancelAll(TAG);
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
     }
 }
--- app/src/main/java/ca/prairesunapplications/evemarkethub/utils/RestCalls.java
+++ app/src/main/java/ca/prairesunapplications/evemarkethub/utils/RestCalls.java
@@ -2,82 +2,170 @@ package ca.prairesunapplications.evemarkethub.utils;
 
 import android.app.ProgressDialog;
 import android.util.Log;
+import android.widget.ImageView;
 
-import com.android.volley.Request;
+import com.android.volley.Cache;
+import com.android.volley.Request.Method;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.google.gson.JsonObject;
+import com.android.volley.toolbox.NetworkImageView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import static ca.prairesunapplications.evemarkethub.utils.AppController.TAG;
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
+import ca.prairesunapplications.evemarkethub.R;
+
 import static ca.prairesunapplications.evemarkethub.utils.AppController.getInstance;
 
 public class RestCalls {
 
-    ProgressDialog pDialog = new ProgressDialog(getInstance());
+    private String TAG = RestCalls.class.getSimpleName();
 
-    public void JsonObjectRequest(String url){
-        // Tag used to cancel the request
-        String tag_json_obj = "json_obj_req";
+    // These tags will be used to cancel the requests
+    private String tag_json_obj = "jobj_req", tag_json_arry = "jarray_req";
+    private ProgressDialog pDialog = new ProgressDialog(getInstance());
+    private NetworkImageView imgNetWorkView;
+    private ImageView imageView;
 
-        pDialog.setMessage("Loading...");
-        pDialog.show();
-
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
-                url, null,
+    /**
+     * Making json object request
+     * */
+    private void makeJsonObjReq() {
+        showProgressDialog();
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Method.GET,
+                Const.URL_JSON_OBJECT, null,
                 new Response.Listener<JSONObject>() {
 
                     @Override
                     public void onResponse(JSONObject response) {
                         Log.d(TAG, response.toString());
-                        pDialog.hide();
+
+                        hideProgressDialog();
                     }
                 }, new Response.ErrorListener() {
 
             @Override
             public void onErrorResponse(VolleyError error) {
                 VolleyLog.d(TAG, "Error: " + error.getMessage());
-                // hide the progress dialog
-                pDialog.hide();
+                hideProgressDialog();
             }
-        });
+        }) {
 
-// Adding request to request queue
-        AppController.getInstance().addToRequestQueue(jsonObjReq, tag_json_obj);
+            /**
+             * Passing some request headers
+             * */
+            @Override
+            public Map<String, String> getHeaders() {
+                HashMap<String, String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
 
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("name", "Androidhive");
+                params.put("email", "abc@androidhive.info");
+                params.put("pass", "password123");
+
+                return params;
+            }
+
+        };
+
+        // Adding request to request queue
+        getInstance().addToRequestQueue(jsonObjReq,
+                tag_json_obj);
+
+        // Cancelling request
+        // ApplicationController.getInstance().getRequestQueue().cancelAll(tag_json_obj);
     }
 
-    public void JsonArrayRequest(String url){
-        // Tag used to cancel the request
-        String tag_json_arry = "json_array_req";
-
-        pDialog.setMessage("Loading...");
-        pDialog.show();
-
-        JsonArrayRequest req = new JsonArrayRequest(url,
+    /**
+     * Making json array request
+     * */
+    private void makeJsonArryReq() {
+        showProgressDialog();
+        JsonArrayRequest req = new JsonArrayRequest(Const.URL_JSON_ARRAY,
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
                         Log.d(TAG, response.toString());
-                        pDialog.hide();
+                        hideProgressDialog();
                     }
                 }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 VolleyLog.d(TAG, "Error: " + error.getMessage());
-                pDialog.hide();
+                hideProgressDialog();
             }
         });
 
-// Adding request to request queue
-        AppController.getInstance().addToRequestQueue(req, tag_json_arry);
+        // Adding request to request queue
+        getInstance().addToRequestQueue(req,tag_json_arry);
 
+        // Cancelling request
+        // ApplicationController.getInstance().getRequestQueue().cancelAll(tag_json_arry);
+    }
+
+    private void makeImageRequest() {
+        ImageLoader imageLoader = AppController.getInstance().getImageLoader();
+
+        // If you are using NetworkImageView
+        imgNetWorkView.setImageUrl(Const.URL_IMAGE, imageLoader);
+
+
+        // If you are using normal ImageView
+		/*imageLoader.get(Const.URL_IMAGE, new ImageListener() {
+
+			@Override
+			public void onErrorResponse(VolleyError error) {
+				Log.e(TAG, "Image Load Error: " + error.getMessage());
+			}
+
+			@Override
+			public void onResponse(ImageContainer response, boolean arg1) {
+				if (response.getBitmap() != null) {
+					// load image into imageview
+					imageView.setImageBitmap(response.getBitmap());
+				}
+			}
+		});*/
+
+        // Loading image with placeholder and error image
+        imageLoader.get(Const.URL_IMAGE, ImageLoader.getImageListener(
+                imageView, R.drawable.ico_loading, R.drawable.ico_error));
+
+        Cache cache = getInstance().getRequestQueue().getCache();
+        Cache.Entry entry = cache.get(Const.URL_IMAGE);
+        if (entry != null) {
+            try {
+                String data = new String(entry.data, "UTF-8");
+                // handle data, like converting it to xml, json, bitmap etc.,
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // cached response doesn't exists. Make a network call here
+        }
     }
 
 
+        private void showProgressDialog() {
+        if (!pDialog.isShowing())
+            pDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (pDialog.isShowing())
+            pDialog.hide();
+    }
 }
